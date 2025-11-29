17c06a42bc2f7f5b0c0708e3de6775a76a27fac7
Aadi <aadiiit2024@gmail.com>
2025-11-29 14:30:16 +0530

    Google import oauth error fixed by removing it

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </GoogleOAuthProvider>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
