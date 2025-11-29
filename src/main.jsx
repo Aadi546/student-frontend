39fd58fea943625bb6c4fd8752a7e5f36c20ed2e
Aadi <aadiiit2024@gmail.com>
2025-11-29 20:37:01 +0530

    add google success useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import "./index.css";
-import App from "./App.jsx";
-
-const clientId = "991222763032-nvlrj4mg3k2pdegrg69gsf7rcb8hptgn.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </StrictMode>
 );
